7ec78378c18801bc81ba0773ffe0a3bddaf197b1
lachezar <l.nikolov@campus.tu-berlin.de>
2022-12-19 00:04:50 +0200

    Handling buttons

--- ad-app/README.md
+++ ad-app/README.md
@@ -2,7 +2,12 @@
 This folder contains currently an example for an HbbTV App with the following features:
 * Every minute display a dummy ad every minute for a period of 20s 
 * This ad could be standard banner od L shaped banner (change ad type in the request for testing the both types)
+* Register events for relevant remote control buttons and prints simple text to the console
 
 
 ## Running the App
 The app has been tested with the Red Orbit HbbTV emulator
+
+## Net planned steps for development
+* Change picture on the dummy ads on pressing a button
+* Initial implementation of the library/SDK for handling the custom DA JSON format
--- ad-app/main.js
+++ ad-app/main.js
@@ -1,38 +1,120 @@
+//
+// This file holds the main functionality of the HbbTV app
+//
 const AD_PERIOD = 1000 * 60 * 1; // 1 minutes period 
 const TYPE_BANNER = "banner";
 const TYPE_L_BANNER = "l-banner";
 
-var current_ad = null;
-var appManager = null;
-var appObject = null;
+// scene implementation
+var scene = {
+    theAppObject:null,
+    appAreaDiv: null,
+    isAdRunning: false,
+    currentAd: null,
+    videoBroadcastDiv: null,
+    initialize: function(appObj) {
+        this.theAppObject = appObj;
+        this.appAreaDiv = document.getElementById('app_area');
+        this.videoBroadcastDiv = document.getElementById('video');
+        // register RC button event listener
+        rcUtils.registerKeyEventListener();
+        // initial state is app_area hidden
+    },
+    resizeVideo: function(ratio) {
+        this.videoBroadcastDiv.style.left = (1280*(1-ratio)).toString() + "px";
+        this.videoBroadcastDiv.style.width = (1280*(ratio)).toString() + "px";
+        this.videoBroadcastDiv.style.height = (720*(ratio)).toString() + "px";
+    },
+    restoreVideo: function() {
+        this.videoBroadcastDiv.video.style.left = "0px";
+        this.videoBroadcastDiv.video.style.width = "1280px";
+        this.videoBroadcastDiv.video.style.height = "720px";
+    }
+};
+
+// RC button press handler function
+function handleKeyCode(kc) {
+    try {
+        // process buttons
+        switch (kc) {
+            case VK_RED:
+                // red button only prints to the console for now
+                console.log("Red");
+                break;
+            case VK_GREEN:
+                // green button only prints to the console for now
+                console.log("Green");
+                break;
+            case VK_YELLOW:
+                // yellow button only prints to the console for now
+                console.log("Yellow");
+                break;
+            case VK_BLUE:
+                // blue button only prints to the console for now
+                console.log("Blue");
+                break;
+            case VK_LEFT:
+                // left button only prints to the console for now
+                console.log("Left");
+                break;
+            case VK_RIGHT:
+                // right button only prints to the console for now
+                console.log("Right");
+                break;
+            case VK_DOWN:
+                // down button only prints to the console for now
+                console.log("Down");
+                break;
+            case VK_UP:
+                // up button only prints to the console for now
+                console.log("Up");
+                break; 
+            default:
+                // pressed unhandled key only prints to the console for now
+                console.log("Other button");
+        }
+    }
+    catch (e) {
+        // handle the error cases
+    }
+    // return true to prevent default action for processed keys
+    return true;
+}
+
 
 // function to called on loading the app
 function start() {
     // attempt to acquire the Application object
     appManager = document.getElementById('applicationManager');
     appObject = appManager.getOwnerApplication(document);
-    appObject.show();
-    let video = document.getElementById("video");
-    // First make request immediately and then set interval
+    if (appObject === null) {
+        // error acquiring the Application object!
+    } 
+    else {
+        // initialize the scene
+        scene.initialize(appObject);
+        appObject.show();
+    }
+    // first make request immediately and then set interval
     requestAd();
     setInterval(requestAd, AD_PERIOD);
 }
 
 function requestAd() {
-    // ToDo: Make VAST Request
+    // ToDo: make VAST Request
 
-    // ToDo: Call async function for waiting for the Response 
-    // Dummy ad data
+    // ToDo: call async function for waiting for the Response 
+    // dummy ad data
     let ad_data = {
         type_ad: TYPE_BANNER,
         duration: 20 * 1000 // 20s
     };
-    // ToDo: Move the following function into the async function
+    // ToDo: move the following function into the async function
     display_ad(ad_data);
 }
 
 function display_ad(ad_data) {
-    // ToDo: Finish the actual functionality
+    // ToDo: finish the actual functionality
     if (ad_data.type_ad === TYPE_BANNER) {
         display_banner(ad_data);
     } else if (ad_data.type_ad === TYPE_L_BANNER) {
@@ -54,11 +136,8 @@ function display_banner(ad_data) {
 
 function display_l_banner(ad_data) {
     document.getElementById("app_area").style.zIndex = "1";
-    let video = document.getElementById("video");
     let ratio = 0.8 // dummy ratio
-    video.style.left = (1280*(1-ratio)).toString() + "px";
-    video.style.width = (1280*(ratio)).toString() + "px";
-    video.style.height = (720*(ratio)).toString() + "px";
+    scene.resizeVideo(ratio);
     let l_banner_div = document.getElementById("l-banner-div");
     l_banner_div.style.display = "block";
     current_ad = TYPE_L_BANNER;
@@ -72,9 +151,7 @@ function hide_ad() {
         current_ad = null;
     } else if (current_ad === TYPE_L_BANNER) {
         let video = document.getElementById("video");
-        video.style.left = "0px";
-        video.style.width = "1280px";
-        video.style.height = "720px";
+        scene.restoreVideo();
         let l_banner_div = document.getElementById("l-banner-div");
         l_banner_div.style.display = "none";
         current_ad = null;

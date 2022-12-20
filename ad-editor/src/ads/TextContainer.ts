96630a12eedddf9f1b7c0688e3143588ca1599ec
Todor Moskov <todor.moskov96@gmail.com>
2022-12-20 14:42:02 +0200

    Add Font Family to TextContainer

--- ad-editor/src/ads/TextContainer.ts
+++ ad-editor/src/ads/TextContainer.ts
@@ -10,6 +10,13 @@ export interface TextContainerProps extends ContainerProps {
     text?: string;
     color?: string;
     textAlign?: string; // center, left, right
+    /**
+     * Font Family examples:
+     *  "Times New Roman", Times, serif;
+     *  Arial, Helvetica, sans-serif;
+     *  "Lucida Console", "Courier New", monospace;
+     */
+    fontFamily?: string;
     fontStyle?: string; // normal, italic
     fontWeight?: string; // normal, bold
     fontSize?: string; // 14px, 30px, 40px

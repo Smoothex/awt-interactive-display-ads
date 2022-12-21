37db05c6770ced87bc618881776db166321d91f1
Todor Moskov <todor.moskov96@gmail.com>
2022-12-21 18:46:56 +0200

    Add lists for the ad templates and ad instances in the ad editor

--- ad-editor/src/App.tsx
+++ ad-editor/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {AppBar, Box, CssBaseline, Icon, Toolbar, Typography} from "@mui/material";
+import {AppBar, Box, CssBaseline, Icon, List, ListItemButton, ListItemText, ListSubheader, Toolbar, Typography} from "@mui/material";
 import 'react-reflex/styles.css'
 import { ReflexContainer, ReflexElement, ReflexSplitter } from 'react-reflex';
 
@@ -8,7 +8,7 @@ function App() {
     return (
         <Box height="100vh" display="flex" flexDirection="column">
             <CssBaseline/>
-            <AppBar>
+            <AppBar style={{ backgroundColor: "#00203fff" }}>
                 <Toolbar>
                     <Icon sx={{ margin: "8px" }}>
                         <img src={process.env.PUBLIC_URL + "/favicon.ico"} alt="Logo" style={{width: "24px", height: "24px"}}/>
@@ -24,29 +24,73 @@ function App() {
                     {/* Left split element for the ad templates and ad instances */}
                     <ReflexElement flex={0.2}>
                         <ReflexContainer orientation="horizontal">
-                            {/* Ad templates */}
+                            {/* Ad Templates */}
                             <ReflexElement>
-                                <div style={{ backgroundColor: "green"}}>
-                                    <label>
-                                        Ad templates
-                                    </label>
-                                </div>
+                                <List
+                                    sx={{ width: '100%', 'min-width': '100px' }}
+                                    component="nav"
+                                    aria-labelledby="nested-list-subheader"
+                                    subheader={
+                                        <ListSubheader component="div" id="nested-list-subheader"
+                                                       style={{ padding: "12px 16px", backgroundColor: "#adefD1ff", color: "#00203fff" }}
+                                        >
+                                            <Typography component="div" variant="h6">
+                                                Ad Templates
+                                            </Typography>
+                                        </ListSubheader>
+                                    }
+                                >
+                                    <ListItemButton>
+                                        <ListItemText primary="First ad template" />
+                                    </ListItemButton>
+                                    <ListItemButton>
+                                        <ListItemText primary="Second ad template" />
+                                    </ListItemButton>
+                                    <ListItemButton>
+                                        <ListItemText primary="Third ad template" />
+                                    </ListItemButton>
+                                    <ListItemButton >
+                                        <ListItemText primary="Fourth ad template" />
+                                    </ListItemButton>
+                                </List>
                             </ReflexElement>
                             <ReflexSplitter/>
                             {/* Ad instances */}
                             <ReflexElement>
-                                <div style={{ backgroundColor: "yellow"}}>
-                                    <label>
-                                        Ad instances
-                                    </label>
-                                </div>
+                                <List
+                                    sx={{ width: '100%', 'min-width': '100px' }}
+                                    component="nav"
+                                    aria-labelledby="nested-list-subheader"
+                                    subheader={
+                                        <ListSubheader component="div" id="nested-list-subheader"
+                                                       style={{ padding: "12px 16px", backgroundColor: "#adefD1ff", color: "#00203fff" }}
+                                        >
+                                            <Typography component="div" variant="h6">
+                                                Ad Instances
+                                            </Typography>
+                                        </ListSubheader>
+                                    }
+                                >
+                                    <ListItemButton>
+                                        <ListItemText primary="First ad instance" />
+                                    </ListItemButton>
+                                    <ListItemButton>
+                                        <ListItemText primary="Second ad instance" />
+                                    </ListItemButton>
+                                    <ListItemButton>
+                                        <ListItemText primary="Third ad instance" />
+                                    </ListItemButton>
+                                    <ListItemButton >
+                                        <ListItemText primary="Fourth ad instance" />
+                                    </ListItemButton>
+                                </List>
                             </ReflexElement>
                         </ReflexContainer>
                     </ReflexElement>
                     <ReflexSplitter/>
                     {/* Right split element for the ad editor */}
                     <ReflexElement>
-                        <div style={{ backgroundColor: "red"}}>
+                        <div>
                             <label>
                                 Ad editor
                             </label>

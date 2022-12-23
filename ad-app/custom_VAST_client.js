8799d5e753147d8456714532a10bb546563ac181
Smoothex <momchildp@gmail.com>
2022-12-23 21:08:30 +0100

    edit VAST client to fit current XML schema

--- ad-app/custom_VAST_client.js
+++ ad-app/custom_VAST_client.js
@@ -12,9 +12,10 @@ function makeVASTRequest(url) {
     let InLine = Ad.getElementsByTagName("InLine")[0];
     let AdTitle = InLine.getElementsByTagName("AdTitle")[0];
     let Description = InLine.getElementsByTagName("Description")[0];
-    let Creative = InLine.getElementsByTagName("Creatives")[0].getElementsByTagName("Creative")[0].getElementsByTagName("NonLinear")[0];
-    let MediaFile = Creative.getElementsByTagName("MediaFiles")[0].getElementsByTagName("MediaFile")[0];
-    let Duration = Creative.getElementsByTagName("Duration")[0];
+    let Creative = InLine.getElementsByTagName("Creatives")[0].getElementsByTagName("Creative")[0];
+    let NonLinear = Creative.getElementsByTagName("NonLinearAds")[0].getElementsByTagName("NonLinear")[0];
+    let StaticResource = NonLinear.getElementsByTagName("StaticResource")[0];
+    let Duration = NonLinear.getElementsByTagName("Duration")[0];
     let durationList = Duration.textContent.split(":");
     let durationSeconds = Number(durationList[0])*3600 + Number(durationList[1]*60) + Number(durationList[2]);
     
@@ -22,7 +23,7 @@ function makeVASTRequest(url) {
       title: AdTitle.textContent,
       description: Description.textContent,
       duration: durationSeconds,
-      url: MediaFile.textContent  
+      url: StaticResource.textContent  
     }
   }
 

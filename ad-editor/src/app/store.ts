283e83146b53e52e5ad349cb52858e8d729f3da4
Todor Moskov <todor.moskov96@gmail.com>
2023-01-22 19:38:04 +0100

    Add redux-persist and configure ad slice persistence

--- ad-editor/src/app/store.ts
+++ ad-editor/src/app/store.ts
@@ -1,14 +1,49 @@
-import {configureStore} from "@reduxjs/toolkit";
+import {
+    combineReducers,
+    configureStore
+} from "@reduxjs/toolkit";
+
+import {
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
+
+import storage from 'redux-persist/lib/storage'
+
 import adReducer from "../features/ad/adSlice";
+
 import diagramReducer from "../features/diagram/diagramSlice";
+
 import dialogReducer from "../features/dialog/dialogSlice";
 
+const rootReducer = combineReducers({
+    ad: adReducer,
+    diagram: diagramReducer,
+    dialog: dialogReducer
+});
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+    whitelist: ["ad"]
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-    reducer: {
-        ad: adReducer,
-        diagram: diagramReducer,
-        dialog: dialogReducer
-    }
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>
--- ad-editor/src/index.tsx
+++ ad-editor/src/index.tsx
@@ -5,6 +5,10 @@ import App from './App';
 import {store} from "./app/store";
 import {Provider} from "react-redux";
 import reportWebVitals from './reportWebVitals';
+import {persistStore} from "redux-persist";
+import {PersistGate} from "redux-persist/integration/react";
+
+let persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,7 +16,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
       <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
           <App />
+        </PersistGate>
       </Provider>
   </React.StrictMode>
 );
